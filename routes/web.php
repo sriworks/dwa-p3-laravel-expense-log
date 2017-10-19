22b6c29719b10ba76eba3031cd31a07b5fb316f1
Sridhar Subramaniam <sriworks@gmail.com>
2017-10-19 00:17:31 +0000

    Local config setup, env routes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::get('/', function () {
 Route::get('/expense', 'ExpenseController@index');
 
 Route::post('/expense', 'ExpenseController@create');
+
+Route::get('/env', function () {
+    dump(config('app.name'));
+    dump(config('app.env'));
+    dump(config('app.debug'));
+    dump(config('app.url'));
+});
